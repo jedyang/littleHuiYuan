7b3257e7244c20a2ea13e1083d71f184a5dab18c
yunsheng <helloyunsheng@126.com>
2018-04-12 21:14:13 +0800

    1

---
+++ pages/cards/cards.js
@@ -0,0 +1,67 @@
+//cards.js
+const app = getApp();
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function (res) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
+    var that = this
+    console.log(that.data.userInfo)
+    wx.request({
+      url: 'http://localhost:8080/api/member/' + that.data.userInfo+'/allMember',
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({ members: res.data.data })
+      }
+    })
+  },
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    var phone = e.detail.value
+    var that = this
+    wx.request({
+      url: 'http://localhost:8080/api/member/1234/allMember/' + phone,
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({ members: res.data.data })
+      }
+    })
+  },
+})
