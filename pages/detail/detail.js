f7f7a81702c3a6afe89467079dec66c7706e2b3f
yunsheng <helloyunsheng@126.com>
2018-07-11 22:11:41 +0800

    youhua

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -6,11 +6,12 @@ Page({
   onLoad: function (res) {
     this.setData({
       memberId: res.memberId,
+      shopId: res.shopId,
     });
-    console.log("detail memberID:" + res.memberId);
+    console.log("detail memberID:" + res.memberId + ",shopId:" + res.shopId);
     var that = this
     wx.request({
-      url: 'http://localhost:8080/api/member/detail?memberId=' + that.data.memberId,
+      url: 'http://localhost:8080/api/member/consumeList?memberId=' + that.data.memberId +"&shopId=" + that.data.shopId,
       method: 'GET',
       header: {
         'content-type': 'application/json', // 默认值
