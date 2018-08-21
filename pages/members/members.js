6d3a3975a6d45b981286f609d50629351c2fd1ec
yunsheng <helloyunsheng@126.com>
2018-08-21 22:03:51 +0800

    会员页面

--- pages/members/members.js
+++ pages/members/members.js
@@ -2,6 +2,8 @@
 const app = getApp();
 Page({
   data: {
+    inputShowed: false,
+    inputVal: ""
   },
   onLoad: function (res) {
     this.setData({
@@ -21,20 +23,40 @@ Page({
       }
     })
   },
-  formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+    // 随时动态查询会员
     var words = e.detail.value
     var that = this
     wx.request({
-      url: 'http://localhost:8080/api/member/allMember?' + 'shopId=1234' + '&words=' + words,
+      url: 'http://localhost:8080/api/member/allMember?shopId=' + that.data.shopId + '&words=' + words,
       method: 'GET',
       header: {
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
-        console.log(res.data)
-        that.setData({ members: res.data.data })
+        console.log(res)
+      
+        that.setData({ members: res.data.result })
       }
     })
-  },
+  }
 })
