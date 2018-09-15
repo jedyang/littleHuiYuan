f31c71a95a931f9c7c5bdffb1644427b1f4f770e
yunsheng <helloyunsheng@126.com>
2018-09-15 21:56:52 +0800

    up

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,8 +5,6 @@ Page({
   },
   onLoad: function (res) {
     this.setData({
-      memberId: res.memberId,
-      shopId: res.shopId,
       cardId: res.cardId
     });
     console.log("detail memberID:" + res.memberId + ",shopId:" + res.shopId + ",cardId:" + res.cardId);
@@ -21,6 +19,18 @@ Page({
         console.log(res.data)
         that.setData({ logs: res.data.result })
       }
-    })
+    });
+    // 查询卡片信息
+    wx.request({
+      url: 'http://localhost:8080/api/cards/cardInfo?cardId=' + that.data.cardId,
+      method: 'GET',
+      header: {
+        'content-type': 'application/json', // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({ baseInfo: res.data.result })
+      }
+    });
   },
 })
