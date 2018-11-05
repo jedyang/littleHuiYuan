c1e79d7dfd4ae3618384f2c65bd7c8ab8bf03441
yunsheng <helloyunsheng@126.com>
2018-11-05 22:18:08 +0800

    up

--- pages/index/index.js
+++ pages/index/index.js
@@ -26,32 +26,6 @@ Page({
   onLoad: function() {
     console.log("view Index onLoad");
     this.getIndexData();
-    // if (app.globalData.userInfo) {
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   })
-    // } else if (this.data.canIUse) {
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       })
-    //     }
-    //   })
-    // }
     // 查询用户信息
     var shopUserId = wx.getStorageSync("shopUserId");
     var memberId = wx.getStorageSync("memberId");
@@ -61,47 +35,6 @@ Page({
       shopUserId: shopUserId,
       memberId: memberId
     })
-    /*
-    if (wx.getStorageSync("isShopUser")) {
-      // 是老板，去到我的店铺页面
-      wx.request({
-        url: 'http://localhost:8080/api/shop/myShops?userId=' + wx.getStorageSync("ShopUserId"),
-        method: 'GET',
-        header: {
-          'content-type': 'application/json' // 默认值
-        },
-        success: function(res) {
-          console.log("index:");
-          console.log(res.data)
-          // 如果只有一个店铺直接展示店铺详情
-          // if(res.data.result.length == 1){
-          //   wx.navigateTo({
-          //     url: '../shopDetail/shopDetail?shopId=' + res.data.result[0].shopId,
-          //   })
-          // }else{
-          //   wx.switchTab({
-          //     url: '../shops/shops',
-          //   })
-          // }
-         
-        }
-      })
-    } else if (wx.getStorageSync("isMember")) {
-      // 是顾客，渠道会员卡页面
-      // wx.request({
-      //   url: 'http://localhost:8080/api/shop/myShops?userId=' + wx.getStorageSync("ShopUserId"),
-      //   method: 'GET',
-      //   header: {
-      //     'content-type': 'application/json' // 默认值
-      //   },
-      //   success: function(res) {
-      //       wx.switchTab({
-      //         url: '../cards/cards',
-      //       })
-      //   }
-      // })
-    }
-*/
   },
   onReady: function() {
     // Do something when page ready.
@@ -119,23 +52,4 @@ Page({
     // Do something when page close.
     console.log("view Index onUnload");
   },
-  // getUserInfo: function(e) {
-  //   console.log(e)
-  //   app.globalData.userInfo = e.detail.userInfo
-  //   this.setData({
-  //     userInfo: e.detail.userInfo,
-  //     hasUserInfo: true
-  //   })
-  // },
-  // doRegiste: function() {
-  //   wx.redirectTo({
-  //     url: '../registe/registe',
-  //   })
-  // },
-
-  // doAddMember: function() {
-  //   wx.redirectTo({
-  //     url: '../registe/registe',
-  //   })
-  // }
 })
